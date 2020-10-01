d9d1a63a38af1e09a3454a590487ae5f31cca7df
jalmeidasolis <jalmeidasolis@gmail.com>
2020-09-30 19:48:13 -0500

    Actualizacion del CRUD con Node.js + MongoDB

--- code/node-project/components/carrera/storage.js
+++ code/node-project/components/carrera/storage.js
@@ -1,27 +1,41 @@
+const db = require('mongoose')
+const model = require('./model')
+
+const uri = "mongodb+srv://ups:ups2020@cluster0.a6aag.gcp.mongodb.net/utups?retryWrites=true&w=majority";
+
+db.Promise = global.Promise
+db.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'utups',
+})
+    .then(() => { console.log( '[db] - Conectada con éxito.' ) })
+    .catch((error) => { console.error( '[error log] - ' + error ) })
+
 const list = []
 
 function addCarrera( objeto ) {
-    list.push( objeto )
+    const carrera = new model( objeto )
+    carrera.save()
 }
 
-function getCarreras() {
-    return list
+async function getCarreras() {
+    const carreraList = await model.find()
+    return carreraList
 }
 
-function updateCarrera( objeto ) {
-    for(let i=0; i<list.length; i++){
-        if(list[i].nombre == objeto.nombre){
-           list[i] = objeto
-        }
-    }
+async function updateCarrera( idCarrera, objeto ) {
+    const foundCarrera = await model.findOne({ _id: idCarrera })
+    foundCarrera.nombre = objeto.nombre
+    foundCarrera.abreviatura = objeto.abreviatura
+    foundCarrera.descripcion = objeto.descripcion
+
+    const result = await foundCarrera.save()
+    return result 
 }
 
-function deleteCarrera( nombre ) {
-    for(let i=0; i<list.length; i++){
-        if(list[i].nombre == nombre ){
-           delete list[i]
-        }
-    }
+function deleteCarrera( idCarrera ) {
+    return model.deleteOne({ _id: idCarrera})
 }
 
 module.exports = {
